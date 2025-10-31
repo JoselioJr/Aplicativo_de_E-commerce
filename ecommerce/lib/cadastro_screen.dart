b0872d3c54f6875c78b7999a43b443df0e95cf71
Joselio Jr. <joselio.fc@hotmail.com>
2025-10-31 13:50:00 -0300

    ✨ feat: Atualização da tela de cadastro de produtos
    Adição do formulário de descrição do produto e da imagem do produto

--- ecommerce/lib/cadastro_screen.dart
+++ ecommerce/lib/cadastro_screen.dart
@@ -1,9 +1,20 @@
+import 'dart:io';
+
 import 'package:ecommerce/models/todo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
 
-class CadastroScreen extends StatelessWidget {
+class CadastroScreen extends StatefulWidget {
+  @override
+  _CadastroScreenState createState() => _CadastroScreenState();
+}
+
+class _CadastroScreenState extends State<CadastroScreen> {
   final TextEditingController _textController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  File? _image;
   
   @override
   Widget build(BuildContext context) {
@@ -13,67 +24,118 @@ class CadastroScreen extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 15, 15, 15),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-              child: TextFormField(
-                controller: _textController,
-                style: TextStyle(fontSize: 18, color: Colors.black),
-                decoration: InputDecoration(
-                  labelText: "Nome do Produto",
-                  labelStyle: TextStyle(fontSize: 18, color: Colors.grey),
-                  border: OutlineInputBorder(),
+      body: Column(
+        children: [
+          GestureDetector(
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              width: 120,
+              height: 120,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.grey,
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1,
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira o nome do produto';
-                  }
-                  return null;
-                },
               ),
+              child: _image == null
+                  ? Icon(Icons.add_a_photo, size: 30)
+                  : (kIsWeb 
+                      ? Image.network(_image!.path, fit: BoxFit.cover, errorBuilder: (context, error, stackTrace) => Icon(Icons.image_not_supported))
+                      : Image.file(_image!, fit: BoxFit.cover)),
             ),
-            Row(
+            onTap: () async {
+              final ImagePicker _picker = ImagePicker();
+              final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+              if (image != null) {
+                setState(() {
+                  _image = File(image.path);
+                });
+              }
+            },
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
               children: [
-                Expanded(child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      FocusScope.of(context).unfocus();
-                      if (_formKey.currentState!.validate()) {
-                        Produto novoProduto = Produto(_textController.text);
-                        Navigator.pop(context, novoProduto);
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                  child: TextFormField(
+                    controller: _textController,
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: "Nome do Produto",
+                      labelStyle: TextStyle(fontSize: 18, color: Colors.grey),
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor, insira o nome do produto';
                       }
+                      return null;
                     },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 64, 187, 7),
-                    ),
-                    child: Text("Salvar", style: TextStyle(fontSize: 18, color: Colors.white)),
                   ),
                 ),
-                ),
-                Expanded(child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      FocusScope.of(context).unfocus();
-                      Navigator.pop(context);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 242, 79, 4),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  child: TextFormField(
+                    controller: _descriptionController,
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: "Descrição do Produto",
+                      labelStyle: TextStyle(fontSize: 18, color: Colors.grey),
+                      border: OutlineInputBorder(),
                     ),
-                    child: Text("Cancelar", style: TextStyle(fontSize: 18, color: Colors.white)),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor, insira a descrição do produto';
+                      }
+                      return null;
+                    },
                   ),
                 ),
-                ),
+                Row(
+                  children: [
+                    Expanded(child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      height: 40,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          FocusScope.of(context).unfocus();
+                          if (_formKey.currentState!.validate()) {
+                            Produto novoProduto = Produto(_textController.text, descricao: _descriptionController.text, image: _image);
+                            Navigator.pop(context, novoProduto);
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 64, 187, 7),
+                        ),
+                        child: Text("Salvar", style: TextStyle(fontSize: 18, color: Colors.white)),
+                      ),
+                    ),
+                    ),
+                    Expanded(child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      height: 40,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          FocusScope.of(context).unfocus();
+                          Navigator.pop(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 242, 79, 4),
+                        ),
+                        child: Text("Cancelar", style: TextStyle(fontSize: 18, color: Colors.white)),
+                      ),
+                    ),
+                    ),
+                  ],
+                )
               ],
             )
-          ],
-        )
+          ),
+        ],
       ),
     );
   }
