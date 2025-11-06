f0a5293c3531deeb78436a657e3cda7bda322d49
Joselio Jr. <joselio.fc@hotmail.com>
2025-11-05 21:51:18 -0300

    🚚 chore: Alteração do nome da classe objeto

--- ecommerce/lib/models/produto.dart
+++ ecommerce/lib/models/produto.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 class Produto {
   late String nome;
   late String descricao;
+  late double valor;
   File? image;
 
-  Produto(this.nome, {this.descricao = "", this.image});
+  Produto(this.nome, {this.descricao = "", this.valor = 0.0, this.image});
 }
